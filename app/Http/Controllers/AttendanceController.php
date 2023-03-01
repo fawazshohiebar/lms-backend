3c23bb1b1f69c837402bf237d05464caba7859a9
fawazshohiebar <fawazsho.132@gmail.com>
2023-03-01 10:36:57 +0200

    controllers

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -25,7 +25,16 @@ class AttendanceController extends Controller
      */
     public function create()
     {
-        //
+        $attendance = Attendance::all();
+        $attendanceData = $attendance->map(function($attendance){
+            return [
+                'id' => $attendance->id,
+                'Date'=> $attendance->Date,
+                'Status'=>$attendance->Status,
+                'Students_ID'=>$attendance->Students_ID,
+            ];
+        });
+            return $attendance;
     }
 
     /**
@@ -36,7 +45,12 @@ class AttendanceController extends Controller
      */
     public function store(StoreattendanceRequest $request)
     {
-        //
+        $attendance = new attendance();
+        $attendance->Date=$request->input('Date');
+        $attendance->Status=$request->input('Status');
+        $attendance->Students_ID=$request->input('Students_ID');
+        $attendance->save();
+        return response()->json(['message' =>'attendance entered successfully' ]);
     }
 
     /**
@@ -68,9 +82,18 @@ class AttendanceController extends Controller
      * @param  \App\Models\attendance  $attendance
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateattendanceRequest $request, attendance $attendance)
+    public function update(UpdateattendanceRequest $request, attendance $attendance,$id)
     {
-        //
+        $attendance = attendance::find($id);
+        if (!$attendance) {
+            return response()->json(['message' => 'attendance not found'], 404);
+        }
+            $attendance->Date = $request->has('Date')? $request->input('Date'):$attendance->Date;
+            $attendance->Status = $request->has('Status')?$request->input('Status'):$attendance->Status;
+            $attendance->Students_ID = $request->has('Students_ID')?$request->input('Students_ID'):$attendance->Students_ID;
+
+            $attendance->save();
+            return response()->json(['message' => 'attendance updated successfully'], 200);   
     }
 
     /**
@@ -79,8 +102,10 @@ class AttendanceController extends Controller
      * @param  \App\Models\attendance  $attendance
      * @return \Illuminate\Http\Response
      */
-    public function destroy(attendance $attendance)
+    public function destroy(attendance $attendance, $id)
     {
-        //
+        $ana = attendance::find($id);
+        $ana->delete();
+        return "the id have been deleted ";
     }
 }
